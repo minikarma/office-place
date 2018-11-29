f0d8ca1249d239c469498cc5b798ed904a5374f4
Andrey Karmatsky <karma@karmas-MacBook-Pro.local>
2018-11-29 12:07:42 +0300

    mousemove fix

--- app.js
+++ app.js
@@ -99,29 +99,28 @@ map.on("load", ()=>{
 
    map.on('mousemove', 'hex', function(e) {
        // Change the cursor style as a UI indicator.
-       map.getCanvas().style.cursor = 'pointer';
+       if(e.features.length>0) {
+         map.getCanvas().style.cursor = 'pointer';
 
-       var coordinates = {lng: e.features[0].properties.lon, lat: e.features[0].properties.lat};
-//       console.log(e.lngLat);
-       //e.lngLat;
-       //[e.features[0].properties.lat,e.features[0].properties.lat];
-       var description = e.features[0].properties[currentMode];
+         var coordinates = {lng: e.features[0].properties.lon, lat: e.features[0].properties.lat};
+  //       console.log(e.lngLat);
+         //e.lngLat;
+         //[e.features[0].properties.lat,e.features[0].properties.lat];
+         var description = e.features[0].properties[currentMode];
 
-       // Ensure that if the map is zoomed out such that multiple
-       // copies of the feature are visible, the popup appears
-       // over the copy being pointed to.
-       while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-           coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+         // Populate the popup and set its coordinates
+         // based on the feature found.
+         popup.setLngLat(coordinates)
+             .setHTML(description)
+             .addTo(map);
+       } else {
+         map.getCanvas().style.cursor = '';
+         popup.remove();
        }
 
-       // Populate the popup and set its coordinates
-       // based on the feature found.
-       popup.setLngLat(coordinates)
-           .setHTML(description)
-           .addTo(map);
    });
 
-   map.on('mouseleave', 'places', function() {
+   map.on('mouseleave', 'hex', function() {
        map.getCanvas().style.cursor = '';
        popup.remove();
    });
